a09e57fa1ba81d3f2f47ed3949d95f998d60f747
Dave <davemate23@gmail.com>
2017-02-25 09:10:40 +0000

    Edited create course migration

---
+++ db/migrate/20170201001224_create_courses.rb
@@ -0,0 +1,56 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+	    t.string :title
+	  	t.integer :engfee
+	  	t.string :pubukprn
+	  	t.string :ukprn, index: true
+	  	t.string :assurl
+	  	t.string :crseurl
+	  	t.boolean :distance
+	  	t.string :employurl
+	  	t.boolean :feetbc
+	  	t.boolean :honours
+	  	t.integer :foundation
+	  	t.string :jacs1
+	  	t.string :jacs2
+	  	t.string :jacs3
+	  	t.string :kiscourseid, index: true
+	  	t.integer :kismode
+	  	t.integer :kistype
+	  	t.string :ldcs1
+	  	t.string :ldcs2
+	  	t.string :ldcs3
+	  	t.string :level
+	  	t.boolean :locchnge
+	  	t.string :lturl
+	  	t.boolean :meanssup
+	  	t.boolean :nhs
+	  	t.integer :nifee
+	  	t.boolean :noncreditassess
+	  	t.integer :numstage
+	  	t.boolean :othsup
+	  	t.string :relatedkis1
+	  	t.string :relatedkis2
+	  	t.string :relatedkis3
+	  	t.integer :sandwich
+	  	t.integer :scotfee
+	  	t.string :supporturl
+	  	t.string :ucasprogid, index: true
+	  	t.string :ukprnapply
+	  	t.integer :varfee
+	  	t.integer :wafee
+	  	t.boolean :waiver
+	  	t.integer :welsh
+	  	t.integer :yearabroad
+	  	t.integer :kisaim
+	  	t.integer :avgwritten
+	  	t.integer :avgcoursework
+	  	t.integer :avgscheduled
+	  	t.references :location, foreign_key: true
+	  	t.references :institute, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
