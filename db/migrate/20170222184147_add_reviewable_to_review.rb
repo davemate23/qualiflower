3b48fa204babefb35471342a0796a5ab448876d0
Dave <davemate23@gmail.com>
2017-02-25 09:20:40 +0000

    Delete review migrations

--- db/migrate/20170222184147_add_reviewable_to_review.rb
+++
@@ -1,5 +0,0 @@
-class AddReviewableToReview < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :reviews, :reviewable, polymorphic: true
-  end
-end
--- db/migrate/20170223114335_rename_users_id_to_user_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameUsersIdToUserId < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :reviews, :users_id, :user_id
-  end
-end
