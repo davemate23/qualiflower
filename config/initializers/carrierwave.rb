fd82d2c41bdf45e8605b613d66a8278649145c27
Dave <davemate23@gmail.com>
2017-02-26 19:33:34 +0000

    Add region to AWS Fog settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["aws_access_key_id"],                        # required
     aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
+    region:                ENV["aws_region"], # required 
   }
   config.fog_directory  = ENV["fog_directory"]                          # required
 end
