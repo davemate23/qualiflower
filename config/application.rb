1993fb7a7cbb5601d973df0c15605f82ebc1553a
Shivareddy <shiva.r547@gmail.com>
2017-03-25 11:15:07 +0530

    commit application

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Qualiflower
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_job.queue_adapter = :delayed_job
+    # config.active_job.queue_adapter = :delayed_job
   end
 end
