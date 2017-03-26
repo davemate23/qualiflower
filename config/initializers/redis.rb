d2b5f997d7ef77ea7f7f9ea0f478197a000a03ea
Shivareddy <shiva.r547@gmail.com>
2017-03-26 23:25:47 +0530

    commit files

--- config/initializers/redis.rb
+++
@@ -1,5 +0,0 @@
-# uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/" if Rails.env = "test"
-
-uri =  "redis://redistogo:d75f4da8cefca8ba1a5652b5b8d3d1c3@jack.redistogo.com:9471/"
-
-REDIS = Redis.new(:url => uri)
